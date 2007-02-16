b13c395160c07a2a8f3614c3c2758a9c2e646fc9
Christian Neukirchen <chneukirchen@gmail.com>
2007-02-16 16:42:00 +0000

    Add more Rack::Utils specs
    darcs-hash:20070216164218-4fc50-758a106831f72bf7cfefef812bb90e3ab40132fb.gz

--- test/spec_rack_utils.rb
+++ test/spec_rack_utils.rb
@@ -1,5 +1,30 @@
 require 'rack/utils'
 
+context "Rack::Utils" do
+  specify "should escape correctly" do
+    Rack::Utils.escape("fo<o>bar").should.equal "fo%3Co%3Ebar"
+    Rack::Utils.escape("a space").should.equal "a+space"
+    Rack::Utils.escape("q1!2\"'w$5&7/z8)?\\").
+      should.equal "q1%212%22%27w%245%267%2Fz8%29%3F%5C"
+  end
+
+  specify "should unescape correctly" do
+    Rack::Utils.unescape("fo%3Co%3Ebar").should.equal "fo<o>bar"
+    Rack::Utils.unescape("a+space").should.equal "a space"
+    Rack::Utils.unescape("a%20space").should.equal "a space"
+    Rack::Utils.unescape("q1%212%22%27w%245%267%2Fz8%29%3F%5C").
+      should.equal "q1!2\"'w$5&7/z8)?\\"
+  end
+
+  specify "should parse queries correctly" do
+    Rack::Utils.parse_query("foo=bar").should.equal "foo" => "bar"
+    Rack::Utils.parse_query("foo=bar&foo=quux").
+      should.equal "foo" => ["bar", "quux"]
+    Rack::Utils.parse_query("my+weird+field=q1%212%22%27w%245%267%2Fz8%29%3F").
+      should.equal "my weird field" => "q1!2\"'w$5&7/z8)?"
+  end
+end
+
 context "Rack::Utils::HeaderHash" do
   specify "should capitalize on all accesses" do
     h = Rack::Utils::HeaderHash.new("foo" => "bar")
