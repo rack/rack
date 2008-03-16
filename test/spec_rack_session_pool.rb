741b414f0e4bbfc9fbc3109c3bbfec36d62c26fc
Scytrin dai Kinthra <scytrin@gmail.com>
2008-03-16 14:31:00 +0000

    updated and addition to tests for pool.rb for expiration and thread safety
    Running the freshness tests sleeps for 4 seconds to allow a session's
    expiration point to pass.
    
    darcs-hash:20080316143145-f4dbf-b1296c26ab0fb9a91a11f81ad711717d7f5c34f2.gz

--- test/spec_rack_session_pool.rb
+++ test/spec_rack_session_pool.rb
@@ -3,6 +3,7 @@ require 'test/spec'
 require 'rack/session/pool'
 require 'rack/mock'
 require 'rack/response'
+require 'thread'
 
 context "Rack::Session::Pool" do
   incrementor = lambda { |env|
@@ -18,7 +19,7 @@ context "Rack::Session::Pool" do
     res.body.should.equal '{"counter"=>1}'
   end
 
-  specify "loads from a cookie" do
+  specify "determines session from a cookie" do
     pool = Rack::Session::Pool.new(incrementor)
     res = Rack::MockRequest.new(pool).get("/")
     cookie = res["Set-Cookie"]
@@ -33,5 +34,51 @@ context "Rack::Session::Pool" do
     res = Rack::MockRequest.new(pool).
       get("/", "HTTP_COOKIE" => "rack.session=blarghfasel")
     res.body.should.equal '{"counter"=>1}'
+  end
+
+  specify "maintains freshness" do
+    pool = Rack::Session::Pool.new(incrementor, :expire_after => 3)
+    res = Rack::MockRequest.new(pool).get('/')
+    res.body.should.include '"counter"=>1'
+    cookie = res["Set-Cookie"]
+    res = Rack::MockRequest.new(pool).get('/', "HTTP_COOKIE" => cookie)
+    res["Set-Cookie"].should.equal cookie
+    res.body.should.include '"counter"=>2'
+    sleep 4
+    res = Rack::MockRequest.new(pool).get('/', "HTTP_COOKIE" => cookie)
+    res["Set-Cookie"].should.not.equal cookie
+    res.body.should.include '"counter"=>1'
+  end
+
+  specify "multithread: should merge sessions" do
+    delta_incrementor = lambda do |env|
+      # emulate disconjoinment of threading
+      env['rack.session'] = env['rack.session'].dup
+      Thread.stop
+      env['rack.session'][(Time.now.usec*rand).to_i] = true
+      incrementor.call(env)
+    end
+    pool = Rack::Session::Pool.new(incrementor)
+    res = Rack::MockRequest.new(pool).get('/')
+    res.body.should.equal '{"counter"=>1}'
+    cookie = res["Set-Cookie"]
+    sess_id = cookie[/#{pool.key}=([^,;]+)/,1]
+
+    pool = pool.context(delta_incrementor)
+    r = Array.new(rand(7).to_i+3).
+      map! do
+        Thread.new do
+          Rack::MockRequest.new(pool).get('/', "HTTP_COOKIE" => cookie)
+        end
+      end.
+      reverse!.
+      map!{|t| t.run.join.value }
+    session = pool.for.pool[sess_id] # for is needed by Utils::Context
+    session.size.should.be r.size+1 # counter
+    session['counter'].should.be 2 # meeeh
+    r.each do |res|
+      res['Set-Cookie'].should.equal cookie
+      res.body.should.include '"counter"=>2'
+    end
   end
 end
