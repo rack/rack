ac5c97d438fe807c2e929189de4aa21f8eafe01d
Scytrin dai Kinthra <scytrin@gmail.com>
2008-06-01 06:34:14 -0700

    Tests for Rack::Directory, as well as removal of Rack::File dependency

--- lib/rack/directory.rb
+++ lib/rack/directory.rb
@@ -6,7 +6,7 @@ module Rack
   #
   # If +app+ is not specified, a Rack::File of the same +root+ will be used.
 
-  class Directory < Rack::File
+  class Directory
     DIR_FILE = "<tr><td class='name'><a href='%s'>%s</a></td><td class='size'>%s</td><td class='type'>%s</td><td class='mtime'>%s</td></tr>"
     DIR_PAGE = <<-PAGE
 <html><head>
@@ -33,15 +33,21 @@ table { width:100%%; }
     PAGE
 
     attr_reader :files
+    attr_accessor :root, :path
 
     def initialize(root, app=nil)
-      super(root)
-      @app = app || Rack::File.new(@root)
+      @root = root
+      @app = app
+      unless defined? @app
+        @app = Rack::File.new(@root)
+      end
     end
 
     def call(env)
       dup._call(env)
     end
+
+    F = ::File
 
     def _call(env)
       if env["PATH_INFO"].include? ".."
@@ -72,17 +78,76 @@ table { width:100%%; }
       end
 
       return [404, {"Content-Type" => "text/plain"},
-              ["Directory not found: #{env["PATH_INFO"]}\n"]]
+              ["Entity not found: #{env["PATH_INFO"]}\n"]]
     end
 
     def each
+      show_path = @path.sub(/^#{@root}/,'')
       files = @files.map{|f| p f; DIR_FILE % f }*"\n"
-      page  = DIR_PAGE % [ @path, @path , files ]
+      page  = DIR_PAGE % [ show_path, show_path , files ]
       page.each_line{|l| yield l }
     end
 
     def each_entry
       @files.each{|e| yield e }
     end
+
+    # From WEBrick.
+    MIME_TYPES = {
+      "ai"    => "application/postscript",
+      "asc"   => "text/plain",
+      "avi"   => "video/x-msvideo",
+      "bin"   => "application/octet-stream",
+      "bmp"   => "image/bmp",
+      "class" => "application/octet-stream",
+      "cer"   => "application/pkix-cert",
+      "crl"   => "application/pkix-crl",
+      "crt"   => "application/x-x509-ca-cert",
+     #"crl"   => "application/x-pkcs7-crl",
+      "css"   => "text/css",
+      "dms"   => "application/octet-stream",
+      "doc"   => "application/msword",
+      "dvi"   => "application/x-dvi",
+      "eps"   => "application/postscript",
+      "etx"   => "text/x-setext",
+      "exe"   => "application/octet-stream",
+      "gif"   => "image/gif",
+      "htm"   => "text/html",
+      "html"  => "text/html",
+      "jpe"   => "image/jpeg",
+      "jpeg"  => "image/jpeg",
+      "jpg"   => "image/jpeg",
+      "js"    => "text/javascript",
+      "lha"   => "application/octet-stream",
+      "lzh"   => "application/octet-stream",
+      "mov"   => "video/quicktime",
+      "mpe"   => "video/mpeg",
+      "mpeg"  => "video/mpeg",
+      "mpg"   => "video/mpeg",
+      "pbm"   => "image/x-portable-bitmap",
+      "pdf"   => "application/pdf",
+      "pgm"   => "image/x-portable-graymap",
+      "png"   => "image/png",
+      "pnm"   => "image/x-portable-anymap",
+      "ppm"   => "image/x-portable-pixmap",
+      "ppt"   => "application/vnd.ms-powerpoint",
+      "ps"    => "application/postscript",
+      "qt"    => "video/quicktime",
+      "ras"   => "image/x-cmu-raster",
+      "rb"    => "text/plain",
+      "rd"    => "text/plain",
+      "rtf"   => "application/rtf",
+      "sgm"   => "text/sgml",
+      "sgml"  => "text/sgml",
+      "tif"   => "image/tiff",
+      "tiff"  => "image/tiff",
+      "txt"   => "text/plain",
+      "xbm"   => "image/x-xbitmap",
+      "xls"   => "application/vnd.ms-excel",
+      "xml"   => "text/xml",
+      "xpm"   => "image/x-xpixmap",
+      "xwd"   => "image/x-xwindowdump",
+      "zip"   => "application/zip",
+    }
   end
 end
