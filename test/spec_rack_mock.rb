de3c04ab513a21282d3625c2fe871f97be3beed2
Matt Todd <chiology@gmail.com>
2009-01-14 10:33:05 -0500

    Fix brittle test after Rack::Request memoization
    The Content-Length changed due to how Rack::Request memoizes
    itself in the env.

--- test/spec_rack_mock.rb
+++ test/spec_rack_mock.rb
@@ -125,7 +125,7 @@ context "Rack::MockResponse" do
     res.original_headers["Content-Type"].should.equal "text/yaml"
     res["Content-Type"].should.equal "text/yaml"
     res.content_type.should.equal "text/yaml"
-    res.content_length.should.be 381  # needs change often.
+    res.content_length.should.be 444  # needs change often.
     res.location.should.be.nil
   end
 
