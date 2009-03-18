939fa7d24c8086ad3b9799704a32a2e30d6fbd4e
TJ Holowaychuk <tj@vision-media.ca>
2009-03-18 16:33:58 -0700

    Refactored Rack::Request#media_type
    No need for subscript, first reads better
    
    Signed-off-by: Christian Neukirchen <chneukirchen@gmail.com>

--- lib/rack/request.rb
+++ lib/rack/request.rb
@@ -46,7 +46,7 @@ module Rack
     # For more information on the use of media types in HTTP, see:
     # http://www.w3.org/Protocols/rfc2616/rfc2616-sec3.html#sec3.7
     def media_type
-      content_type && content_type.split(/\s*[;,]\s*/, 2)[0].downcase
+      content_type && content_type.split(/\s*[;,]\s*/, 2).first.downcase
     end
 
     # The media type parameters provided in CONTENT_TYPE as a Hash, or
