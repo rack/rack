f9d38d9d866060be39192675adcc2efc19164c6f
Jeremy Kemper <jeremy@bitsweat.net>
2009-04-21 23:43:20 -0700

    Work in tandem with ContentLength. Avoid dup per request.

--- test/spec_rack_commonlogger.rb
+++ test/spec_rack_commonlogger.rb
@@ -8,25 +8,42 @@ require 'rack/mock'
 context "Rack::CommonLogger" do
   app = lambda { |env|
     [200,
+     {"Content-Type" => "text/html", "Content-Length" => length.to_s},
+     [obj]]}
+  app_without_length = lambda { |env|
+    [200,
      {"Content-Type" => "text/html"},
-     ["foo"]]}
+     []]}
+  app_with_zero_length = lambda { |env|
+    [200,
+     {"Content-Type" => "text/html", "Content-Length" => "0"},
+     []]}
 
   specify "should log to rack.errors by default" do
-    log = StringIO.new
     res = Rack::MockRequest.new(Rack::CommonLogger.new(app)).get("/")
 
     res.errors.should.not.be.empty
-    res.errors.should =~ /GET /
-    res.errors.should =~ / 200 / # status
-    res.errors.should =~ / 3 /   # length
+    res.errors.should =~ /"GET \/ " 200 #{length} /
   end
 
-  specify "should log to anything with <<" do
-    log = ""
+  specify "should log to anything with +write+" do
+    log = StringIO.new
     res = Rack::MockRequest.new(Rack::CommonLogger.new(app, log)).get("/")
 
-    log.should =~ /GET /
-    log.should =~ / 200 / # status
-    log.should =~ / 3 / # length
+    log.string.should =~ /"GET \/ " 200 #{length} /
+  end
+
+  specify "should log - content length if header is missing" do
+    res = Rack::MockRequest.new(Rack::CommonLogger.new(app_without_length)).get("/")
+
+    res.errors.should.not.be.empty
+    res.errors.should =~ /"GET \/ " 200 - /
+  end
+
+  specify "should log - content length if header is zero" do
+    res = Rack::MockRequest.new(Rack::CommonLogger.new(app_with_zero_length)).get("/")
+
+    res.errors.should.not.be.empty
+    res.errors.should =~ /"GET \/ " 200 - /
   end
 end
