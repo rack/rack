fc6320a84cab34c2f7576f68224e745f7815b559
raggi <jftucker@gmail.com>
2010-03-23 19:49:28 +0000

    Update Rack::Mock to use Rack::VERSION too

--- lib/rack/mock.rb
+++ lib/rack/mock.rb
@@ -40,7 +40,7 @@ module Rack
     end
 
     DEFAULT_ENV = {
-      "rack.version" => [1,1],
+      "rack.version" => Rack::VERSION,
       "rack.input" => StringIO.new,
       "rack.errors" => StringIO.new,
       "rack.multithread" => true,
