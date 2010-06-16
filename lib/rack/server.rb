rack/rack

commit b433e484c9ee47ffceb7645129403df31f60db7f

set ENV["RACK_ENV"] to options[:environment] as many frameworks rely on this

lib/rack/server.rb
@@ -226,6 +226,7 @@ module Rack
         args.clear if ENV.include?("REQUEST_METHOD")
 
         options.merge! opt_parser.parse! args
+        ENV["RACK_ENV"] = options[:environment]
         options
       end
 

commit 32de46c139e10dc2261ca63a3049e98e310d0781

Rack should respect $RACK_ENV when starting up, fixes 3rd party bugs introduced by b433e484c9ee47ffceb7645129403df31f60db7f

lib/rack/server.rb
@@ -144,7 +144,7 @@ module Rack
 
     def default_options
       {
-        :environment => "development",
+        :environment => ENV['RACK_ENV'] || "development",
         :pid         => nil,
         :Port        => 9292,
         :Host        => "0.0.0.0",