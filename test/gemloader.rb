90cca4837fa6bcf299f5c7c4561c192a3887a16c
raggi <jftucker@gmail.com>
2011-02-09 18:32:26 -0800

    improve gemloader to include runtime deps if any, and not break on complex requirements

--- test/gemloader.rb
+++ test/gemloader.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
 project = 'rack'
 gemspec = File.expand_path("#{project}.gemspec", Dir.pwd)
-eval(File.read(gemspec)).development_dependencies.each do |dep|
-  gem dep.name, dep.requirement.to_s
+Gem::Specification.load(gemspec).dependencies.each do |dep|
+  gem dep.name, *dep.requirement.as_list
 end
