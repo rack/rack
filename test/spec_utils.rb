0700cd04c41cb2e0882521fcf2fdca877b94e0ae
John Firebaugh <john.firebaugh@gmail.com>
2011-04-10 16:10:09 -0700

    Just use CGI.escape/unescape.
    The previous definitions were virtually identical to those provided
    by CGI in 1.8.7/1.9.2, except that Rack::Utils.escape failed on non-
    UTF8 encoded input.
    
    Tested on 1.8.7, 1.9.2 and JRuby.

--- test/spec_utils.rb
+++ test/spec_utils.rb
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 require 'rack/utils'
 require 'rack/mock'
 
@@ -16,6 +18,12 @@ describe Rack::Utils do
     matz_name_sep = "\xE3\x81\xBE\xE3\x81\xA4 \xE3\x82\x82\xE3\x81\xA8".unpack("a*")[0] # Matsu moto
     matz_name_sep.force_encoding("UTF-8") if matz_name_sep.respond_to? :force_encoding
     Rack::Utils.escape(matz_name_sep).should.equal '%E3%81%BE%E3%81%A4+%E3%82%82%E3%81%A8'
+  end
+
+  if "".respond_to?(:encode)
+    should "escape non-UTF8 strings" do
+      Rack::Utils.escape("ø".encode("ISO-8859-1")).should.equal "%F8"
+    end
   end
 
   should "unescape correctly" do
