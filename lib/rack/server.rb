bfd95a6005a87d864bcecd69951c892d80575554
raggi <jftucker@gmail.com>
2011-05-02 22:10:25 -0700

    shurrup 1.8.6

--- lib/rack/server.rb
+++ lib/rack/server.rb
@@ -247,7 +247,7 @@ module Rack
         # http://hoohoo.ncsa.uiuc.edu/cgi/cl.html
         args.clear if ENV.include?("REQUEST_METHOD")
 
-        options.merge! opt_parser.parse! args
+        options.merge! opt_parser.parse!(args)
         options[:config] = ::File.expand_path(options[:config])
         ENV["RACK_ENV"] = options[:environment]
         options
