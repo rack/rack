rack/rack

commit f043f32ce99c060d5bfac6e974cc050d1c327161

Restore Ruby 1.9.1 compatibility
URI.decode_www_form_component and URI.encode_www_form_component were not
added until Ruby 1.9.2.

lib/rack/utils.rb
@@ -1,12 +1,12 @@
 # -*- encoding: binary -*-
-
 require 'fileutils'
 require 'set'
 require 'tempfile'
-
 require 'rack/multipart'
 
-if RUBY_VERSION[/^\d+\.\d+/] == '1.8'
+major, minor, patch = RUBY_VERSION.split('.')
+
+if (major == 1 && minor < 9) || (major == 1 && minor == 9 && patch < 2)
   # pull in backports
   require 'rack/backports/uri/common'
 else

test/spec_utils.rb
@@ -1,5 +1,4 @@
 # -*- encoding: utf-8 -*-
-
 require 'rack/utils'
 require 'rack/mock'
 

commit 97ee1dea85472f6264de64389af12cd80f95b3f6

Fix a regression caused by f043f32ce99

lib/rack/utils.rb
@@ -4,7 +4,7 @@ require 'set'
 require 'tempfile'
 require 'rack/multipart'
 
-major, minor, patch = RUBY_VERSION.split('.')
+major, minor, patch = RUBY_VERSION.split('.').map { |v| v.to_i }
 
 if (major == 1 && minor < 9) || (major == 1 && minor == 9 && patch < 2)
   # pull in backports