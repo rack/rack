4eab6501a205021a40c4dfa394f5819f7248f8bb
oleg dashevskii <olegdashevskii@gmail.com>
2011-05-26 11:50:08 +0700

    Fix Rack::Utils.escape in the case when $KCODE='U'
    CGI.escape backport used in this case

--- test/spec_utils.rb
+++ test/spec_utils.rb
@@ -19,6 +19,28 @@ describe Rack::Utils do
     Rack::Utils.escape(matz_name_sep).should.equal '%E3%81%BE%E3%81%A4+%E3%82%82%E3%81%A8'
   end
 
+  if RUBY_VERSION[/^\d+\.\d+/] == '1.8'
+    should "escape correctly for multibyte characters if $KCODE is set to 'U'" do
+      default_kcode, $KCODE = $KCODE, 'U'
+
+      matz_name = "\xE3\x81\xBE\xE3\x81\xA4\xE3\x82\x82\xE3\x81\xA8".unpack("a*")[0] # Matsumoto
+      matz_name.force_encoding("UTF-8") if matz_name.respond_to? :force_encoding
+      Rack::Utils.escape(matz_name).should.equal '%E3%81%BE%E3%81%A4%E3%82%82%E3%81%A8'
+      matz_name_sep = "\xE3\x81\xBE\xE3\x81\xA4 \xE3\x82\x82\xE3\x81\xA8".unpack("a*")[0] # Matsu moto
+      matz_name_sep.force_encoding("UTF-8") if matz_name_sep.respond_to? :force_encoding
+      Rack::Utils.escape(matz_name_sep).should.equal '%E3%81%BE%E3%81%A4+%E3%82%82%E3%81%A8'
+
+      $KCODE = default_kcode
+    end
+
+    should "unescape multibyte characters correctly if $KCODE is set to 'U'" do
+      default_kcode, $KCODE = $KCODE, 'U'
+      Rack::Utils.unescape('%E3%81%BE%E3%81%A4+%E3%82%82%E3%81%A8').should.equal(
+        "\xE3\x81\xBE\xE3\x81\xA4 \xE3\x82\x82\xE3\x81\xA8".unpack("a*")[0])
+      $KCODE = default_kcode
+    end
+  end
+
   if "".respond_to?(:encode)
     should "escape non-UTF8 strings" do
       Rack::Utils.escape("ø".encode("ISO-8859-1")).should.equal "%F8"
