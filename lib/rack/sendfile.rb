0caa3850241a935ae6ebfd6c22696c88381ee34e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2011-06-29 10:21:14 -0700

    Minor error in documentation regarding the order of parameters in HTTP_X_ACCEL_MAPPING.
    Conflicts:
    
    	lib/rack/sendfile.rb

--- lib/rack/sendfile.rb
+++ lib/rack/sendfile.rb
@@ -52,10 +52,9 @@ module Rack
   #   }
   #
   # Note that the X-Sendfile-Type header must be set exactly as shown above. The
-  # X-Accel-Mapping header should specify the name of the private URL pattern,
-  # followed by an equals sign (=), followed by the location on the file system
-  # that it maps to. The middleware performs a simple substitution on the
-  # resulting path.
+  # X-Accel-Mapping header should specify the internal URI path, followed by an
+  # equals sign (=), followed name of the location in the file system that it maps
+  # to. The middleware performs a simple substitution on the resulting path.
   #
   # See Also: http://wiki.codemongers.com/NginxXSendfile
   #
