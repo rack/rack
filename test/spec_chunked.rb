d1cf7167118a85ad67ed266b81e08c7a349ca212
James Tucker <jftucker@gmail.com>
2011-12-21 18:50:39 -0400

    Squash the warning that is output by defining a shadow on Enumerator.

--- test/spec_chunked.rb
+++ test/spec_chunked.rb
@@ -3,7 +3,7 @@ require 'rack/lint'
 require 'rack/mock'
 
 describe Rack::Chunked do
-  Enumerator = ::Enumerator rescue ::Enumerable::Enumerator
+  Enumerator = ::Enumerable::Enumerator unless defined?(Enumerator)
 
   def chunked(app)
     proc do |env|
