a46b05edbdfd19b59a1e9ccc274a01dd52e41273
James Tucker <jftucker@gmail.com>
2011-12-21 19:30:37 -0400

    Make fulltest work on jruby

--- test/gemloader.rb
+++ test/gemloader.rb
@@ -2,5 +2,7 @@ require 'rubygems'
 project = 'rack'
 gemspec = File.expand_path("#{project}.gemspec", Dir.pwd)
 Gem::Specification.load(gemspec).dependencies.each do |dep|
-  gem dep.name, *dep.requirement.as_list
+  unless dep.name == 'fcgi' && RUBY_PLATFORM =~ /java/
+    gem dep.name, *dep.requirement.as_list
+  end
 end
