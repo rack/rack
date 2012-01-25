5498ecff32ae8c72acc1d4baba9fe281656d01a8
Aaron Patterson <aaron.patterson@gmail.com>
2012-01-25 11:25:17 -0800

    Backported fixes for:
      http://bugs.ruby-lang.org/issues/5925
    
    Relevant commit:
    
      https://github.com/ruby/ruby/commit/edb7cdf1eabaff78dfa5ffedfbc2e91b29fa9ca1

--- lib/rack/backports/uri/common_192.rb
+++ lib/rack/backports/uri/common_192.rb
@@ -17,6 +17,22 @@
 require 'uri/common'
 
 module URI
+  256.times do |i|
+    TBLENCWWWCOMP_[i.chr] = '%%%02X' % i
+  end
+  TBLENCWWWCOMP_[' '] = '+'
+  TBLENCWWWCOMP_.freeze
+
+  256.times do |i|
+    h, l = i>>4, i&15
+    TBLDECWWWCOMP_['%%%X%X' % [h, l]] = i.chr
+    TBLDECWWWCOMP_['%%%x%X' % [h, l]] = i.chr
+    TBLDECWWWCOMP_['%%%X%x' % [h, l]] = i.chr
+    TBLDECWWWCOMP_['%%%x%x' % [h, l]] = i.chr
+  end
+  TBLDECWWWCOMP_['+'] = ' '
+  TBLDECWWWCOMP_.freeze
+
   def self.decode_www_form(str, enc=Encoding::UTF_8)
     return [] if str.empty?
     unless /\A#{WFKV_}=#{WFKV_}(?:[;&]#{WFKV_}=#{WFKV_})*\z/o =~ str
