0f6ee067c7414e86fa3c6702b6135de42c29c6c2
brainopia <brainopia@evilmartians.com>
2012-01-27 15:07:08 +0400

    Refactor Rack::Session::Cookie#unpacked_cookie_data

--- lib/rack/session/cookie.rb
+++ lib/rack/session/cookie.rb
@@ -105,14 +105,7 @@ module Rack
 
           if @secrets.size > 0 && session_data
             session_data, digest = session_data.split("--")
-
-            if session_data && digest
-              ok = @secrets.any? do |secret|
-                digest == generate_hmac(session_data, secret)
-              end
-            end
-
-            session_data = nil unless ok
+            session_data = nil unless digest_match?(session_data, digest)
           end
 
           coder.decode(session_data) || {}
@@ -150,6 +143,13 @@ module Rack
       def destroy_session(env, session_id, options)
         # Nothing to do here, data is in the client
         generate_sid unless options[:drop]
+      end
+
+      def digest_match?(data, digest)
+        return unless data && digest
+        @secrets.any? do |secret|
+          digest == generate_hmac(data, secret)
+        end
       end
 
       def generate_hmac(data, secret)
