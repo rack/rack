c9f65df37a151821eb88ddd1dc404b83e52c52d5
James Tucker <jftucker@gmail.com>
2012-05-13 15:02:17 -0700

    Fix parsing performance for unquoted filenames
    Special thanks to Paul Rogers & Eric Wong
    
    Conflicts:
    	test/spec_multipart.rb

--- test/spec_multipart.rb
+++ test/spec_multipart.rb
@@ -295,4 +295,24 @@ describe Rack::Multipart do
       message.should.equal "value must be a Hash"
   end
 
+  should "parse very long unquoted multipart file names" do
+    data = <<-EOF
+--AaB03x\r
+Content-Type: text/plain\r
+Content-Disposition: attachment; name=file; filename=#{'long' * 100}\r
+\r
+contents\r
+--AaB03x--\r
+    EOF
+
+    options = {
+      "CONTENT_TYPE" => "multipart/form-data; boundary=AaB03x",
+      "CONTENT_LENGTH" => data.length.to_s,
+      :input => StringIO.new(data)
+    }
+    env = Rack::MockRequest.env_for("/", options)
+    params = Rack::Utils::Multipart.parse_multipart(env)
+
+    params["file"][:filename].should.equal('long' * 100)
+  end
 end
