a564cf865dca6f7e8080802df1d245f395f986bd
comboy <kacper.ciesla@gmail.com>
2012-10-12 12:57:25 +0200

    fix passing addional options to thin versions below 0.8.0

--- lib/rack/handler/thin.rb
+++ lib/rack/handler/thin.rb
@@ -8,7 +8,10 @@ module Rack
       def self.run(app, options={})
         host = options.delete(:Host) || '0.0.0.0'
         port = options.delete(:Port) || 8080
-        server = ::Thin::Server.new(host, port, app, options)
+        args = [host, port, app, options]
+        # Thin versions below 0.8.0 do not support additional options
+        args.pop if ::Thin::VERSION::MAJOR < 1 && ::Thin::VERSION::MINOR < 8
+        server = ::Thin::Server.new(*args)
         yield server if block_given?
         server.start
       end
