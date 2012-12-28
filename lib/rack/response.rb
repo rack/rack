379a91838c98ce7d4e0e70f92cf34cc8dcb33685
James Tucker <raggi@google.com>
2012-12-28 17:36:36 -0500

    Rack::Response now conforms to body.close SPEC
     * Previously 204, 205 and 304 bodies were not closed correctly.

--- lib/rack/response.rb
+++ lib/rack/response.rb
@@ -74,6 +74,7 @@ module Rack
       if [204, 205, 304].include?(status.to_i)
         header.delete "Content-Type"
         header.delete "Content-Length"
+        close
         [status.to_i, header, []]
       else
         [status.to_i, header, BodyProxy.new(self){}]
