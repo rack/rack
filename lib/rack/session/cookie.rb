fd8ef712cffcd59d95a534d507e8b54f3130b142
Charles Hornberger <charles.hornberger@gmail.com>
2013-09-16 19:12:28 +0200

    oops. this should make 1.8.7 happier

--- lib/rack/session/cookie.rb
+++ lib/rack/session/cookie.rb
@@ -135,7 +135,8 @@ module Rack
           session_data = request.cookies[@key]
 
           if @secrets.size > 0 && session_data
-            *tmp, digest = session_data.split("--")
+            tmp = session_data.split("--")
+            digest = tmp.pop
             session_data = tmp.join("--")
             session_data = nil unless digest_match?(session_data, digest)
           end
