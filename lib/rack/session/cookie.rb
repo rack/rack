ae360063714cc8da548770f70b4a2e280a62256a
Aaron Patterson <aaron.patterson@gmail.com>
2013-12-05 11:49:34 -0800

    use split 2 and reverse

--- lib/rack/session/cookie.rb
+++ lib/rack/session/cookie.rb
@@ -135,9 +135,9 @@ module Rack
           session_data = request.cookies[@key]
 
           if @secrets.size > 0 && session_data
-            tmp = session_data.split("--")
-            digest = tmp.pop
-            session_data = tmp.join("--")
+            digest, session_data = session_data.reverse.split("--", 2)
+            digest.reverse! if digest
+            session_data.reverse! if session_data
             session_data = nil unless digest_match?(session_data, digest)
           end
 
