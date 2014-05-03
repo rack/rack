a9298045466a5e4bb92ac180252881448978db70
Rogério Zambon <rogeriozambon@gmail.com>
2014-05-03 00:42:09 -0300

    Refactoring class and added the module environment.

--- lib/rack/handler/thin.rb
+++ lib/rack/handler/thin.rb
@@ -1,32 +1,46 @@
 require "thin"
 require "rack/content_length"
 require "rack/chunked"
+require "rack/handler/environment"
 
 module Rack
   module Handler
     class Thin
-      def self.run(app, options={})
-        environment  = ENV['RACK_ENV'] || 'development'
-        default_host = environment == 'development' ? 'localhost' : '0.0.0.0'
+      extend Environment
 
-        host = options.delete(:Host) || default_host
-        port = options.delete(:Port) || 8080
-        args = [host, port, app, options]
-        # Thin versions below 0.8.0 do not support additional options
-        args.pop if ::Thin::VERSION::MAJOR < 1 && ::Thin::VERSION::MINOR < 8
-        server = ::Thin::Server.new(*args)
+      def self.run(app, options={})
+        @app = app
+        @options = options
+
         yield server if block_given?
+
         server.start
       end
 
       def self.valid_options
-        environment  = ENV['RACK_ENV'] || 'development'
-        default_host = environment == 'development' ? 'localhost' : '0.0.0.0'
-
         {
-          "Host=HOST" => "Hostname to listen on (default: #{default_host})",
+          "Host=HOST" => "Hostname to listen on (default: #{environment})",
           "Port=PORT" => "Port to listen on (default: 8080)",
         }
+      end
+
+      private
+      def self.prepare_args
+        host = @options.delete(:Host) || environment
+        port = @options.delete(:Port) || 8080
+        args = [host, port, @app, @options]
+
+        args.pop if has_not_support_for_additional_options?
+        args
+      end
+
+      def self.server
+        @server ||= ::Thin::Server.new(*prepare_args)
+      end
+
+      def self.has_not_support_for_additional_options?
+        # Thin versions below 0.8.0 do not support additional options
+        ::Thin::VERSION::MAJOR < 1 && ::Thin::VERSION::MINOR < 8
       end
     end
   end
