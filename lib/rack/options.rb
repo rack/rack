17d00d0ef206cf201ae2f4e370c1d82af5e2deba
AlexKVal <alexkval@gmail.com>
2014-08-25 20:58:01 +0300

    begin/end are useless when all method body is within their scope

--- lib/rack/options.rb
+++ lib/rack/options.rb
@@ -101,25 +101,23 @@ module Rack
     end
 
     def handler_opts(options)
-      begin
-        info = []
-        server = Rack::Handler.get(options[:server]) || Rack::Handler.default(options)
-        if server && server.respond_to?(:valid_options)
-          info << ""
-          info << "Server-specific options for #{server.name}:"
+      info = []
+      server = Rack::Handler.get(options[:server]) || Rack::Handler.default(options)
+      if server && server.respond_to?(:valid_options)
+        info << ""
+        info << "Server-specific options for #{server.name}:"
 
-          has_options = false
-          server.valid_options.each do |name, description|
-            next if name.to_s.match(/^(Host|Port)[^a-zA-Z]/) # ignore handler's host and port options, we do our own.
-            info << "  -O %-21s %s" % [name, description]
-            has_options = true
-          end
-          return "" if !has_options
+        has_options = false
+        server.valid_options.each do |name, description|
+          next if name.to_s.match(/^(Host|Port)[^a-zA-Z]/) # ignore handler's host and port options, we do our own.
+          info << "  -O %-21s %s" % [name, description]
+          has_options = true
         end
-        info.join("\n")
-      rescue NameError, LoadError
-        return "Warning: Could not find handler specified (#{options[:server] || 'default'}) to determine handler-specific options"
+        return "" if !has_options
       end
+      info.join("\n")
+    rescue NameError, LoadError
+      return "Warning: Could not find handler specified (#{options[:server] || 'default'}) to determine handler-specific options"
     end
   end
 
