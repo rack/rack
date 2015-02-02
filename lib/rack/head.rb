15bbd0c2e38bf6ccdf82b35e23c474ef95fc60df
Santiago Pastorino <santiago@wyeworks.com>
2015-02-02 19:34:51 -0200

    Remove whitespaces

--- lib/rack/head.rb
+++ lib/rack/head.rb
@@ -1,17 +1,16 @@
 require 'rack/body_proxy'
 
 module Rack
-  
   # Rack::Head returns an empty body for all HEAD requests. It leaves
   # all other requests unchanged.
   class Head
     def initialize(app)
       @app = app
     end
-  
+
     def call(env)
       status, headers, body = @app.call(env)
-  
+
       if env[REQUEST_METHOD] == HEAD
         [
           status, headers, Rack::BodyProxy.new([]) do
