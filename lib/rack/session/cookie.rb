cd6f35bdf16ee6d2e51c6ca1f2b8a71a95047f71
Francesco Rodríguez <frodsan@protonmail.ch>
2016-05-19 11:03:42 +0200

    Rename :hmac to :digest in session middleware.

--- lib/rack/session/cookie.rb
+++ lib/rack/session/cookie.rb
@@ -105,7 +105,7 @@ module Rack
 
       def initialize(app, options={})
         @secrets = options.values_at(:secret, :old_secret).compact
-        @hmac = options.fetch(:hmac, OpenSSL::Digest::SHA1)
+        @digest_class = options.fetch(:digest, OpenSSL::Digest::SHA1)
 
         warn <<-MSG unless secure?(options)
         SECURITY WARNING: No secret option provided to Rack::Session::Cookie.
@@ -182,7 +182,7 @@ module Rack
       end
 
       def generate_hmac(data, secret)
-        OpenSSL::HMAC.hexdigest(@hmac.new, secret, data)
+        OpenSSL::HMAC.hexdigest(@digest_class.new, secret, data)
       end
 
       def secure?(options)
