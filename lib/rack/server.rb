90fe8f3d6e26b421067505cf8369c43d0d26164c
khotta <khotta116@gmail.com>
2016-10-20 02:21:25 +0900

    accept multiple libraries to require at start method

--- lib/rack/server.rb
+++ lib/rack/server.rb
@@ -264,7 +264,13 @@ module Rack
       end
 
       if library = options[:require]
-        require library
+        if library.is_a?(Array)
+          library.each do |library|
+            require library
+          end
+        else
+          require library
+        end
       end
 
       if options[:debug]
