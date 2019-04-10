f1626ad17126c243454c2bf1cb70cf79b6ee8f8c
lanzhiheng <lanzhihengrj@gmail.com>
2019-04-10 18:08:42 +0800

    Add some unit testing for `condition_get.rb`.

--- test/spec_conditional_get.rb
+++ test/spec_conditional_get.rb
@@ -10,6 +10,10 @@ describe Rack::ConditionalGet do
     Rack::Lint.new Rack::ConditionalGet.new(app)
   end
 
+  def instance_get(app)
+    Rack::ConditionalGet.new(app)
+  end
+
   it "set a 304 status and truncate body when If-Modified-Since hits" do
     timestamp = Time.now.httpdate
     app = conditional_get(lambda { |env|
@@ -102,4 +106,63 @@ describe Rack::ConditionalGet do
     response.body.must_equal 'TEST'
   end
 
+  describe 'private method' do
+    it "fresh? using without any header" do
+      instance = instance_get(lambda { |env| [200, {}, []]})
+      instance.send(:fresh?, {}, {}).must_equal false
+    end
+    it "fresh? using HTTP_IF_NONE_MATCH and Etag" do
+      instance = instance_get(lambda { |env| [200, {}, []]})
+      instance.send(:fresh?, {
+                      'HTTP_IF_NONE_MATCH' => '1234'
+                    }, {
+                      'ETag' => '1234'
+                    }).must_equal true
+      assert_nil instance.send(:fresh?, {
+                      'HTTP_IF_NONE_MATCH' => '4321'
+                    }, {
+                      'Etag' => '1234'
+                    })
+    end
+    it "fresh? using If-Modified-Since and Last-Modified" do
+      current = Time.now.httpdate
+      previous_time = (Time.now - 10000).httpdate
+      future_time = (Time.now + 10000).httpdate
+      instance = instance_get(lambda { |env| [200, {}, []]})
+      instance.send(:fresh?, {
+                      'HTTP_IF_MODIFIED_SINCE' => previous_time
+                    }, {
+                      'Last-Modified' => current
+                    }).must_equal false
+      instance.send(:fresh?, {
+                      'HTTP_IF_MODIFIED_SINCE' => future_time
+                    }, {
+                      'Last-Modified' => current
+                    }).must_equal true
+    end
+
+    it "to_rfc2822 " do
+      instance = instance_get(lambda { |env| [200, {}, []]})
+      assert_nil instance.send(:to_rfc2822, ('wrong time string'))
+      # Not RFC 2822 compliant date
+      assert_nil instance.send(:to_rfc2822, Time.now.gmtime.to_s)
+      instance.send(:to_rfc2822, Time.now.httpdate).must_equal Time.rfc2822(Time.now.httpdate)
+    end
+
+    it "etag_matches?" do
+      instance = instance_get(lambda { |env| [200, {}, []]})
+      instance.send(:etag_matches?, '1234', { 'ETag' => '1234' }).must_equal true
+      instance.send(:etag_matches?, '1234', { 'ETag' => '1233' }).must_equal false
+    end
+
+    it "modified_since?" do
+      current = Time.now.httpdate
+      previous_time = (Time.now - 10000).httpdate
+      future_time = (Time.now + 10000).httpdate
+      instance = instance_get(lambda { |env| [200, {}, []]})
+      instance.send(:modified_since?, Time.rfc2822(current), { 'Last-Modified' => current }).must_equal true
+      instance.send(:modified_since?, Time.rfc2822(future_time), { 'Last-Modified' => current }).must_equal true
+      instance.send(:modified_since?, Time.rfc2822(previous_time), { 'Last-Modified' => current }).must_equal false
+    end
+  end
 end
