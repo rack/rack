3232f9370d099e784a16c01d32e8a2da4a953f18
Rafael Mendonça França <rafael@franca.dev>
2019-10-16 14:07:36 -0400

    Add a version prefix to the private id to make easier to migrate old values

--- lib/rack/session/abstract/id.rb
+++ lib/rack/session/abstract/id.rb
@@ -21,6 +21,8 @@ module Rack
   module Session
 
     class SessionId
+      ID_VERSION = 2
+
       attr_reader :public_id
 
       def initialize(public_id)
@@ -28,7 +30,7 @@ module Rack
       end
 
       def private_id
-        hash_sid public_id
+        "#{ID_VERSION}::#{hash_sid(public_id)}"
       end
 
       alias :cookie_value :public_id
