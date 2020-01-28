905a22deadb65b15e91aa86ed24793fab73b74f5
Jeremy Evans <code@jeremyevans.net>
2020-01-28 14:41:55 -0800

    Add covering tests for abstract session support
    3316 / 3380 LOC (98.11%) covered

--- test/spec_session_abstract_id.rb
+++ test/spec_session_abstract_id.rb
@@ -30,4 +30,53 @@ describe Rack::Session::Abstract::ID do
     id.send(:generate_sid).must_equal 'fake_hex'
   end
 
+  it "should warn when subclassing" do
+    verbose = $VERBOSE
+    begin
+      $VERBOSE = true
+      warn_arg = nil
+      @id.define_singleton_method(:warn) do |arg|
+        warn_arg = arg
+      end
+      c = Class.new(@id)
+      regexp = /is inheriting from Rack::Session::Abstract::ID.  Inheriting from Rack::Session::Abstract::ID is deprecated, please inherit from Rack::Session::Abstract::Persisted instead/
+      warn_arg.must_match(regexp)
+
+      warn_arg = nil
+      c = Class.new(c)
+      warn_arg.must_be_nil
+    ensure
+      $VERBOSE = verbose
+      @id.singleton_class.send(:remove_method, :warn)
+    end
+  end
+
+  it "#find_session should find session in request" do
+    id = @id.new(nil)
+    def id.get_session(env, sid)
+      [env['rack.session'], generate_sid]
+    end
+    req = Rack::Request.new('rack.session' => {})
+    session, sid = id.find_session(req, nil)
+    session.must_equal({})
+    sid.must_match(/\A\h+\z/)
+  end
+
+  it "#write_session should write session to request" do
+    id = @id.new(nil)
+    def id.set_session(env, sid, session, options)
+      [env, sid, session, options]
+    end
+    req = Rack::Request.new({})
+    id.write_session(req, 1, 2, 3).must_equal [{}, 1, 2, 3]
+  end
+
+  it "#delete_session should remove session from request" do
+    id = @id.new(nil)
+    def id.destroy_session(env, sid, options)
+      [env, sid, options]
+    end
+    req = Rack::Request.new({})
+    id.delete_session(req, 1, 2).must_equal [{}, 1, 2]
+  end
 end
--- test/spec_session_abstract_session_hash.rb
+++ test/spec_session_abstract_session_hash.rb
@@ -16,26 +16,64 @@ describe Rack::Session::Abstract::SessionHash do
         true
       end
     end
-    @hash = Rack::Session::Abstract::SessionHash.new(store.new, nil)
+    @class = Rack::Session::Abstract::SessionHash
+    @hash = @class.new(store.new, nil)
   end
 
-  it "returns keys" do
+  it ".find finds entry in request" do
+    assert_equal({}, @class.find(Rack::Request.new('rack.session'=>{})))
+  end
+
+  it ".set sets session in request" do
+    req = Rack::Request.new({})
+    @class.set(req, {})
+    req.env['rack.session'].must_equal({})
+  end
+
+  it ".set_options sets session options in request" do
+    req = Rack::Request.new({})
+    h = {}
+    @class.set_options(req, h)
+    opts = req.env['rack.session.options']
+    opts.must_equal(h)
+    opts.wont_be_same_as(h)
+  end
+
+  it "#keys returns keys" do
     assert_equal ["foo", "baz", "x"], hash.keys
   end
 
-  it "returns values" do
+  it "#values returns values" do
     assert_equal [:bar, :qux, { y: 1 }], hash.values
   end
 
-  describe "#dig" do
-    it "operates like Hash#dig" do
-      assert_equal({ y: 1 }, hash.dig("x"))
-      assert_equal(1, hash.dig(:x, :y))
-      assert_nil(hash.dig(:z))
-      assert_nil(hash.dig(:x, :z))
-      lambda { hash.dig(:x, :y, :z) }.must_raise TypeError
-      lambda { hash.dig }.must_raise ArgumentError
+  it "#dig operates like Hash#dig" do
+    assert_equal({ y: 1 }, hash.dig("x"))
+    assert_equal(1, hash.dig(:x, :y))
+    assert_nil(hash.dig(:z))
+    assert_nil(hash.dig(:x, :z))
+    lambda { hash.dig(:x, :y, :z) }.must_raise TypeError
+    lambda { hash.dig }.must_raise ArgumentError
+  end
+
+  it "#each iterates over entries" do
+    a = []
+    @hash.each do |k,v|
+      a << [k, v]
     end
+    a.must_equal [["foo", :bar], ["baz", :qux], ["x", { y: 1 }]]
+  end
+
+  it "#has_key returns whether the key is in the hash" do
+    assert_equal true, hash.has_key?("foo")
+    assert_equal true, hash.has_key?(:foo)
+    assert_equal false, hash.has_key?("food")
+    assert_equal false, hash.has_key?(:food)
+  end
+
+  it "#replace replaces hash" do
+    hash.replace({bar: "foo"})
+    assert_equal "foo", hash["bar"]
   end
 
   describe "#fetch" do
@@ -56,9 +94,7 @@ describe Rack::Session::Abstract::SessionHash do
     end
   end
 
-  describe "#stringify_keys" do
-    it "returns hash or session hash with keys stringified" do
-      assert_equal({ "foo" => :bar, "baz" => :qux, "x" => { y: 1 } }, hash.send(:stringify_keys, hash).to_h)
-    end
+  it "#stringify_keys returns hash or session hash with keys stringified" do
+    assert_equal({ "foo" => :bar, "baz" => :qux, "x" => { y: 1 } }, hash.send(:stringify_keys, hash).to_h)
   end
 end
