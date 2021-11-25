16bb4308916ac135e4db5d21054149b9669c4837
Steven Bloch <steven.bloch@zendesk.com>
2021-11-24 17:25:18 -0800

    Update regex to match on 127.0.0.0/8

--- lib/rack/request.rb
+++ lib/rack/request.rb
@@ -16,7 +16,7 @@ module Rack
       attr_accessor :ip_filter
     end
 
-    self.ip_filter = lambda { |ip| /\A127\.0\.0\.1\Z|\A(10|172\.(1[6-9]|2[0-9]|30|31)|192\.168)\.|\A::1\Z|\Afd[0-9a-f]{2}:.+|\Alocalhost\Z|\Aunix\Z|\Aunix:/i.match?(ip) }
+    self.ip_filter = lambda { |ip| /\A(127|10|172\.(1[6-9]|2[0-9]|30|31)|192\.168)\.|\A::1\Z|\Afd[0-9a-f]{2}:.+|\Alocalhost\Z|\Aunix\Z|\Aunix:/i.match?(ip) }
     ALLOWED_SCHEMES = %w(https http wss ws).freeze
     SCHEME_WHITELIST = ALLOWED_SCHEMES
     if Object.respond_to?(:deprecate_constant)
