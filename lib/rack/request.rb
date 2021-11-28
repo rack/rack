32e63e83777eaa50f820f324827a3b1ccedb2c43
Steven Bloch <steven.bloch@zendesk.com>
2021-11-28 13:44:04 -0800

    remove $

--- lib/rack/request.rb
+++ lib/rack/request.rb
@@ -19,12 +19,12 @@ module Rack
     valid_ipv4_octet = '(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])'.freeze
     
     TRUSTED_PROXIES = Regexp.union(
-      /\A127(\.(#{valid_ipv4_octet})){3}$\z/,                         # localhost IPv4 range 127.x.x.x, per RFC-3330
-      /\A::1$\z/,                                                      # localhost IPv6 ::1
-      /\Af[cd][0-9a-f]{2}(?::[0-9a-f]{0,4}){0,7}$\z/i,             # private IPv6 range fc00 .. fdff
-      /\A10(\.(#{valid_ipv4_octet})){3}$\z/,                          # private IPv4 range 10.x.x.x
-      /\A172\.(1[6-9]|2[0-9]|3[01])(\.(#{valid_ipv4_octet})){2}$\z/,     # private IPv4 range 172.16.0.0 .. 172.31.255.255
-      /\A192\.168(\.(#{valid_ipv4_octet})){2}$\z/,                     # private IPv4 range 192.168.x.x
+      /\A127(\.(#{valid_ipv4_octet})){3}\z/,                         # localhost IPv4 range 127.x.x.x, per RFC-3330
+      /\A::1\z/,                                                      # localhost IPv6 ::1
+      /\Af[cd][0-9a-f]{2}(?::[0-9a-f]{0,4}){0,7}\z/i,             # private IPv6 range fc00 .. fdff
+      /\A10(\.(#{valid_ipv4_octet})){3}\z/,                          # private IPv4 range 10.x.x.x
+      /\A172\.(1[6-9]|2[0-9]|3[01])(\.(#{valid_ipv4_octet})){2}\z/,     # private IPv4 range 172.16.0.0 .. 172.31.255.255
+      /\A192\.168(\.(#{valid_ipv4_octet})){2}\z/,                     # private IPv4 range 192.168.x.x
       /\Alocalhost\z|\Aunix\z|\Aunix:/i,                              # localhost hostname, and unix domain sockets
     ).freeze
     
