ec2350e0250637c532273fbd186ee764f6f2ac35
Steven Bloch <steven.bloch@zendesk.com>
2021-11-28 18:23:53 -0800

    Line up comments, remove superfluous freeze

--- lib/rack/request.rb
+++ lib/rack/request.rb
@@ -19,14 +19,14 @@ module Rack
     valid_ipv4_octet = /\.(25[0-5]|2[0-4][0-9]|[01]?[0-9]?[0-9])/
     
     trusted_proxies = Regexp.union(
-      /\A127#{valid_ipv4_octet}{3}\z/,                         # localhost IPv4 range 127.x.x.x, per RFC-3330
-      /\A::1\z/,                                                      # localhost IPv6 ::1
-      /\Af[cd][0-9a-f]{2}(?::[0-9a-f]{0,4}){0,7}\z/i,             # private IPv6 range fc00 .. fdff
-      /\A10#{valid_ipv4_octet}{3}\z/,                          # private IPv4 range 10.x.x.x
-      /\A172\.(1[6-9]|2[0-9]|3[01])#{valid_ipv4_octet}{2}\z/,     # private IPv4 range 172.16.0.0 .. 172.31.255.255
+      /\A127#{valid_ipv4_octet}{3}\z/,                          # localhost IPv4 range 127.x.x.x, per RFC-3330
+      /\A::1\z/,                                                # localhost IPv6 ::1
+      /\Af[cd][0-9a-f]{2}(?::[0-9a-f]{0,4}){0,7}\z/i,           # private IPv6 range fc00 .. fdff
+      /\A10#{valid_ipv4_octet}{3}\z/,                           # private IPv4 range 10.x.x.x
+      /\A172\.(1[6-9]|2[0-9]|3[01])#{valid_ipv4_octet}{2}\z/,   # private IPv4 range 172.16.0.0 .. 172.31.255.255
       /\A192\.168#{valid_ipv4_octet}{2}\z/,                     # private IPv4 range 192.168.x.x
-      /\Alocalhost\z|\Aunix(\z|:)/i,                              # localhost hostname, and unix domain sockets
-    ).freeze
+      /\Alocalhost\z|\Aunix(\z|:)/i,                            # localhost hostname, and unix domain sockets
+    )
     
     self.ip_filter = lambda { |ip| trusted_proxies.match?(ip) }
 
