e23b9eb0650c57cd23af8517170f146d7b296e3a
Jeremy Evans <code@jeremyevans.net>
2022-01-24 16:28:38 -0800

    Add comment regarding the conditional override of YAML.unsafe_load

--- test/spec_mock.rb
+++ test/spec_mock.rb
@@ -3,6 +3,9 @@
 require_relative 'helper'
 require 'yaml'
 
+# Work correctly with older versions of Psych, having
+# unsafe_load call load (in older versions, load operates
+# as unsafe_load in current version).
 unless YAML.respond_to?(:unsafe_load)
   def YAML.unsafe_load(body)
     load(body)
