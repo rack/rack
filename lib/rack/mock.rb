4eb407ea27800bb4cdcc60f7714b3aa3030ddd0e
Jeremy Evans <code@jeremyevans.net>
2022-05-10 10:48:11 -0700

    Remove unnecessary conditional in rack/mock.rb
    uri.path is always set to a non-empty value starting with /
    earlier in this method.

--- lib/rack/mock.rb
+++ lib/rack/mock.rb
@@ -116,7 +116,7 @@ module Rack
       env[SERVER_PORT]     = (uri.port ? uri.port.to_s : "80").b
       env[SERVER_PROTOCOL] = opts[:http_version] || 'HTTP/1.1'
       env[QUERY_STRING]    = (uri.query.to_s).b
-      env[PATH_INFO]       = ((!uri.path || uri.path.empty?) ? "/" : uri.path).b
+      env[PATH_INFO]       = (uri.path).b
       env[RACK_URL_SCHEME] = (uri.scheme || "http").b
       env[HTTPS]           = (env[RACK_URL_SCHEME] == "https" ? "on" : "off").b
 
