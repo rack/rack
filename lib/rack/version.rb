f7e9e7fb8c01681345c64e3ac9cd94978c4b2f07
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-08-02 16:37:07 +1200

    Potential compatibility shims.

--- lib/rack/version.rb
+++ lib/rack/version.rb
@@ -13,17 +13,34 @@
 
 module Rack
   # The Rack protocol version number implemented.
-  VERSION = "3.0.0"
+  VERSION = String.new("3.0.0")
+
+  def VERSION.join(separator)
+    warn "Rack::Version is now a String, use it directly!", uplevel: 1
+    VERSION.split('.').join(separator)
+  end
+
+  def VERSION.[](index)
+    warn "Rack::Version is now a String, use it directly!", uplevel: 1
+    VERSION.split('.')[index]
+  end
+
+  VERSION.freeze
 
   # Return the Rack protocol version as a dotted string.
   def self.version
+    # In a future release, say, 3.1?
+    # warn "Rack::VERSION is now a String, use it directly!", uplevel: 1
     VERSION
   end
 
   RELEASE = VERSION
-
+  deprecate_constant :RELEASE
+  
   # Return the Rack release as a dotted string.
   def self.release
+    # In a future release, say, 3.1?
+    # warn "Rack::VERSION is now a String, use it directly!", uplevel: 1
     VERSION
   end
 end
