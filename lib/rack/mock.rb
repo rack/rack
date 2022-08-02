be3d53f32307c2fed363a85770bf1cb93b5e9f4e
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2022-08-02 15:31:16 +1200

    Remove deprecation warning.

--- lib/rack/mock.rb
+++ lib/rack/mock.rb
@@ -1,4 +1,3 @@
 # frozen_string_literal: true
 
-warn "require 'rack/mock' is deprecated and will be removed in Rack 3.1, use require 'rack/mock_request' instead.", uplevel: 1
 require_relative 'mock_request'
