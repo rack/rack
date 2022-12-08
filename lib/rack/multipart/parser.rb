5f6e2fcbbdbff2dfaa21baa693e9d23d12ac1459
John Hawthorn <john@hawthorn.email>
2022-12-08 15:54:28 -0800

    Limit all multipart parts, not just files
    Previously we would limit the number of multipart parts which were
    files, but not other parts. In some cases this could cause parsing of
    maliciously crafted inputs to take longer than expected.
    
    [CVE-2023-27530]

--- lib/rack/multipart/parser.rb
+++ lib/rack/multipart/parser.rb
@@ -3,6 +3,7 @@ require 'rack/utils'
 module Rack
   module Multipart
     class MultipartPartLimitError < Errno::EMFILE; end
+    class MultipartTotalPartLimitError < StandardError; end
 
     class Parser
       BUFSIZE = 16384
@@ -138,7 +139,8 @@ module Rack
           end
 
           @mime_parts[mime_index] = klass.new(body, head, filename, content_type, name)
-          check_open_files
+
+          check_part_limits
         end
 
         def on_mime_body mime_index, content
@@ -150,11 +152,21 @@ module Rack
 
         private
 
-        def check_open_files
-          if Utils.multipart_part_limit > 0
-            if @open_files >= Utils.multipart_part_limit
+        def check_part_limits
+          file_limit = Utils.multipart_file_limit
+          part_limit = Utils.multipart_total_part_limit
+
+          if file_limit && file_limit > 0
+            if @open_files >= file_limit
               @mime_parts.each(&:close)
               raise MultipartPartLimitError, 'Maximum file multiparts in content reached'
+            end
+          end
+
+          if part_limit && part_limit > 0
+            if @mime_parts.size >= part_limit
+              @mime_parts.each(&:close)
+              raise MultipartTotalPartLimitError, 'Maximum total multiparts in content reached'
             end
           end
         end
