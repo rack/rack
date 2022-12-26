ac892ab84dbac20729fcc96da8fcdb5eb4aa7bb9
Wei Zhe <tech@weizheheng.com>
2022-12-26 19:48:01 +0800

    Fix Regexp deprecated third argument with Regexp::NOENCODING (#1998)

--- lib/rack/urlmap.rb
+++ lib/rack/urlmap.rb
@@ -35,7 +35,7 @@ module Rack
         end
 
         location = location.chomp('/')
-        match = Regexp.new("^#{Regexp.quote(location).gsub('/', '/+')}(.*)", nil, 'n')
+        match = Regexp.new("^#{Regexp.quote(location).gsub('/', '/+')}(.*)", Regexp::NOENCODING)
 
         [host, location, match, app]
       }.sort_by do |(host, location, _, _)|
