31ee3c9f2d0ba04d3eb16ebb571ac55e1e4068e6
Patrik Ragnarsson <patrik@starkast.net>
2023-04-18 22:36:42 +0200

    Limit file extension length of multipart tempfiles
    Close https://github.com/rack/rack/issues/1968

--- lib/rack/multipart/parser.rb
+++ lib/rack/multipart/parser.rb
@@ -59,7 +59,9 @@ module Rack
       BUFSIZE = 1_048_576
       TEXT_PLAIN = "text/plain"
       TEMPFILE_FACTORY = lambda { |filename, content_type|
-        Tempfile.new(["RackMultipart", ::File.extname(filename.gsub("\0", '%00'))])
+        extension = ::File.extname(filename.gsub("\0", '%00'))[0, 129]
+
+        Tempfile.new(["RackMultipart", extension])
       }
 
       class BoundedIO # :nodoc:
