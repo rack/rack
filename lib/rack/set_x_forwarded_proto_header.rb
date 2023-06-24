4be88b224304405fdc4f2401d7f445d373bdfc78
Tom Harvey <tom@alush.co.uk>
2023-06-24 19:42:12 +0000

    transforming the header string

--- lib/rack/set_x_forwarded_proto_header.rb
+++ lib/rack/set_x_forwarded_proto_header.rb
@@ -12,7 +12,7 @@ module Rack
   # For example, AWS Cloudfront sets a CloudFront-Forwarded-Proto header
   # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/adding-cloudfront-headers.html#cloudfront-headers-other
   #
-  # Rails usage:
+  # Example Rails usage with AWS Cloudfront:
   # In this AWS example, add 
   #`config.middleware.use Rack::SetXForwardedProtoHeader, 'CloudFront-Forwarded-Proto'`
   # to your application.rb file
@@ -20,7 +20,7 @@ module Rack
   class SetXForwardedProtoHeader
     def initialize(app, vendor_forwarded_header)
       @app = app
-      @vendor_forwarded_header = "HTTP_#{vendor_forwarded_header.upcase}"
+      @vendor_forwarded_header = standard_header vendor_forwarded_header
     end
 
     def call(env)
@@ -33,6 +33,12 @@ module Rack
 
     def copy_header_value(env)
       env["HTTP_X_FORWARDED_PROTO"] = env[@vendor_forwarded_header]
+    end
+
+    def standard_header(header)
+      # Rack expects to see UPPER_UNDERSCORED_HEADERS, never SnakeCased-Dashed-Headers
+      upper_underscored = header.upcase.gsub "-", "_"
+      return "HTTP_#{upper_underscored}"
     end
   end
 end
