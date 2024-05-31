b4985adaefd08992cf33c35da4ec415c1b41c227
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-05-31 12:27:05 +0200

    Don't use deprecated `URI.regexp` (#2184)

--- lib/rack/lint.rb
+++ lib/rack/lint.rb
@@ -12,7 +12,7 @@ module Rack
 
   class Lint
     REQUEST_PATH_ORIGIN_FORM = /\A\/[^#]*\z/
-    REQUEST_PATH_ABSOLUTE_FORM = /\A#{URI::regexp}\z/
+    REQUEST_PATH_ABSOLUTE_FORM = /\A#{URI::DEFAULT_PARSER.make_regexp}\z/
     REQUEST_PATH_AUTHORITY_FORM = /\A(.*?)(:\d*)\z/
     REQUEST_PATH_ASTERISK_FORM = '*'
 
