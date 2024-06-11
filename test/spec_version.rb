520425e558d0cf20bd0f1a1aca158fc5c9522ae8
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-06-12 01:12:51 +0900

    Add more version tests.

--- test/spec_version.rb
+++ test/spec_version.rb
@@ -7,9 +7,21 @@ separate_testing do
 end
 
 describe Rack do
-  describe 'version' do
+  describe 'VERSION' do
     it 'is a version string' do
       Rack::VERSION.must_match(/\d+\.\d+\.\d+/)
+    end
+  end
+
+  describe 'RELEASE' do
+    it 'is the same as VERSION' do
+      Rack::RELEASE.must_equal Rack::VERSION
+    end
+  end
+
+  describe '.release' do
+    it 'returns the version string' do
+      Rack.release.must_equal Rack::VERSION
     end
   end
 end
