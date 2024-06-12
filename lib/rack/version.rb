e2020c155201e48dfcd87cd1d9cfa1b439fb66be
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-06-12 16:24:15 +0900

    Bump patch version.

--- lib/rack/version.rb
+++ lib/rack/version.rb
@@ -12,7 +12,7 @@
 # so it should be enough just to <tt>require 'rack'</tt> in your code.
 
 module Rack
-  RELEASE = "3.1.2"
+  RELEASE = "3.1.3"
 
   # Return the Rack release as a dotted string.
   def self.release
