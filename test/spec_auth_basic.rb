1f5d109e7c8435bedffb1cc0a2026e7f4d012349
Jeremy Evans <code@jeremyevans.net>
2024-07-05 12:25:16 -0700

    Deprecate Rack::Auth::AbstractRequest#request
    This method was added in f80df3903f6b4e97051560382e0481a4754e4218,
    but only for Digest support.  Digest authentication was removed
    in Rack 3.1, so there is no need for this method.
    
    Found by coverage testing.

--- test/spec_auth_basic.rb
+++ test/spec_auth_basic.rb
@@ -100,4 +100,8 @@ describe Rack::Auth::Basic do
     app = Rack::Auth::Basic.new(unprotected_app, realm) { true }
     realm.must_equal app.realm
   end
+
+  deprecated "supports #request for a Rack::Request object" do
+    Rack::Auth::Basic::Request.new({}).request.must_be_kind_of Rack::Request
+  end
 end
