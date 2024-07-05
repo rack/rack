00cae674894bb4cca68c0f0db2fa5985e1288b03
Jeremy Evans <code@jeremyevans.net>
2024-07-05 14:28:25 -0700

    Fix mock request spec to run on Ruby 2.4

--- test/spec_mock_request.rb
+++ test/spec_mock_request.rb
@@ -56,12 +56,14 @@ describe Rack::MockRequest do
   end
 
   it "should convert :input IO object to binary encoding" do
-    f = File.open(__FILE__, :encoding=>'UTF-8')
-    env = Rack::MockRequest.env_for("/", method: :post, input: f)
-    f.external_encoding.must_equal Encoding::BINARY
-    env['rack.input'].read.must_equal File.binread(__FILE__)
-  ensure
-    f&.close
+    begin
+      f = File.open(__FILE__, :encoding=>'UTF-8')
+      env = Rack::MockRequest.env_for("/", method: :post, input: f)
+      f.external_encoding.must_equal Encoding::BINARY
+      env['rack.input'].read.must_equal File.binread(__FILE__)
+    ensure
+      f&.close
+    end
   end
 
   it "return an environment with a path" do
