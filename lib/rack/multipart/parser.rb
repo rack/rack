b3c2a3fb55e17fe476db5943d6925632c95757dd
Jeremy Evans <code@jeremyevans.net>
2024-07-05 12:50:55 -0700

    Remove Multipart::Parser#dequote private method
    The only call of this method was removed in
    51b0c26bedf3b876188ff11d011d5c84e4941c49.
    
    This method was only added a couple years ago, when Digest support
    was removed in 253fc5b29c240276d3cf573bdf795e6a6126b16f (it previously
    called Rack::Auth::Digest::Params::dequote). So it seems unlikely
    there are external users, and therefore it should not need
    deprecation.
    
    Found by coverage testing.

--- lib/rack/multipart/parser.rb
+++ lib/rack/multipart/parser.rb
@@ -249,12 +249,6 @@ module Rack
 
       private
 
-      def dequote(str) # From WEBrick::HTTPUtils
-        ret = (/\A"(.*)"\Z/ =~ str) ? $1 : str.dup
-        ret.gsub!(/\\(.)/, "\\1")
-        ret
-      end
-
       def read_data(io, outbuf)
         content = io.read(@bufsize, outbuf)
         handle_empty_content!(content)
