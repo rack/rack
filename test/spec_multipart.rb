aed514df37e33907df3c971ed3ca9a0a20ac2901
John Hawthorn <john@hawthorn.email>
2024-08-13 16:23:49 -0700

    Fix ReDoS and consistency in multipart regexes
    [CVE-2025-49007]
    
    There is a ReDoS in multipart parsing here because it is not anchored to
    the start of a line and so may match as part of its comments.
    
    Previously in f92e0563f766eade567054e6312a142a2167cb18 Content-ID and
    Content-Type were changed to only accept tab and space as whitespace
    characters. Although that's what the various RFCs show as their BNF,
    I that's supposed to be interpreted _after_ lines have been unfolded and
    so we need to allow FWS "Foldable White Space".
    
    CR is not allowed unescaped as part of quoted-string. It might be
    technically valid with a leading backslash, but I don't believe that
    case is worth supporting.
    
    Co-authored-by: Matthew Draper <matthew@trebex.net>

--- test/spec_multipart.rb
+++ test/spec_multipart.rb
@@ -334,7 +334,7 @@ describe Rack::Multipart do
 
   it "ignores content-disposition values over to 1536 bytes" do
     x = content_disposition_parse.call("a=#{'a'*1510}; filename=\"bar\"; name=\"file\"")
-    x.must_equal "text/plain"=>[""]
+    x.must_equal "application/pdf"=>[""]
   end
 
   it 'raises an EOF error on content-length mismatch' do
@@ -955,7 +955,7 @@ true\r
     data = <<-EOF
 --AaB03x\r
 content-type: text/plain\r
-content-disposition: attachment; name="quoted\\\\chars\\"in\rname"\r
+content-disposition: attachment; name="quoted\\\\chars\\"in\tname"\r
 \r
 true\r
 --AaB03x--\r
@@ -968,7 +968,7 @@ true\r
     }
     env = Rack::MockRequest.env_for("/", options)
     params = Rack::Multipart.parse_multipart(env)
-    params["quoted\\chars\"in\rname"].must_equal 'true'
+    params["quoted\\chars\"in\tname"].must_equal 'true'
   end
 
   it "supports mixed case metadata" do
