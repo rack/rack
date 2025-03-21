8019b8b5107eafb37dd994333285664b1531cd38
Jeremy Evans <code@jeremyevans.net>
2025-03-21 16:41:10 -0700

    Change SPEC to require request environment keys to be strings
    Lint previously would raise for most non-String values, since it
    called:
    
      key.include?(".")
    
    on each environment key.  However, it's best to state this explicitly
    and add a specific Lint check for it.

--- lib/rack/lint.rb
+++ lib/rack/lint.rb
@@ -134,9 +134,14 @@ module Rack
       ##
       ## Incoming HTTP requests are represented using an environment. \
       def check_environment(env)
-        ## The environment must be an unfrozen instance of +Hash+. The Rack application is free to modify the environment, but the modified environment should also comply with this specification.
+        ## The environment must be an unfrozen instance of +Hash+. The Rack application is free to modify the environment, but the modified environment should also comply with this specification. \
         raise LintError, "env #{env.inspect} is not a Hash, but #{env.class}" unless env.kind_of? Hash
         raise LintError, "env should not be frozen, but is" if env.frozen?
+
+        ## All environment keys must be strings.
+        unless env.keys.all?(String)
+          raise LintError, "env contains non-string keys"
+        end
 
         ##
         ## === CGI Variables
