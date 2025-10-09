5d859a9b9d4fd62bbf61c00be4a54c33941aca37
Lewis Buckley <lewis@37signals.com>
2025-10-09 23:01:05 +0100

    Simplify and remove superfluous test

--- test/spec_server_timing.rb
+++ test/spec_server_timing.rb
@@ -34,28 +34,4 @@ describe Rack::ServerTiming do
     response = server_timing_app(app, "db").call(request)
     response[1]['server-timing'].must_match(/^db;dur=[\d\.]+$/)
   end
-
-  it "allows multiple timers to be set" do
-    app = lambda { |env| sleep 0.1; [200, { 'content-type' => 'text/plain' }, "Hello, World!"] }
-    server_timing = server_timing_app(app, "db")
-
-    # wrap many times to guarantee a measurable difference
-    100.times do |i|
-      server_timing = Rack::ServerTiming.new(server_timing, "t#{i}")
-    end
-    server_timing = Rack::ServerTiming.new(server_timing, "total")
-
-    response = server_timing.call(request)
-    server_timing_header = response[1]['server-timing']
-
-    # Verify total value is greater than db value
-    db_duration = server_timing_header.match(/db;dur=([\d\.]+)/)[1]
-    total_duration = server_timing_header.match(/total;dur=([\d\.]+)/)[1]
-    Float(total_duration).must_be :>, Float(db_duration)
-
-    # Verify all other timers are present
-    100.times do |i|
-      server_timing_header.must_match(/t#{i};dur=[\d\.]+/)
-    end
-  end
 end
