93158088efa4cbafcc78c10f1a5f6a19bcc1f4cc
Sai Asish Y <say.apm35@gmail.com>
2026-06-02 16:20:55 -0700

    Support streaming bodies in Rack::Deflater
    Signed-off-by: Sai Asish Y <say.apm35@gmail.com>

--- test/spec_deflater.rb
+++ test/spec_deflater.rb
@@ -109,6 +109,28 @@ describe Rack::Deflater do
     end
   end
 
+  it 'be able to gzip streaming bodies that respond to call' do
+    app_body = Object.new
+    class << app_body
+      def call(stream)
+        stream.write('foo')
+        stream.write('bar')
+      ensure
+        stream.close
+      end
+    end
+
+    status, headers, body = build_response(200, app_body, 'gzip')
+    status.must_equal 200
+    headers['content-encoding'].must_equal 'gzip'
+    body.respond_to?(:each).must_equal false
+    body.respond_to?(:call).must_equal true
+
+    out = StringIO.new(String.new)
+    body.call(out)
+    Zlib::GzipReader.new(StringIO.new(out.string)).read.must_equal 'foobar'
+  end
+
   it 'should not update vary response header if it includes * or accept-encoding' do
     verify(200, 'foobar', deflate_or_gzip, 'response_headers' => { 'vary' => 'Accept-Encoding' } ) do |status, headers, body|
       headers['vary'].must_equal 'Accept-Encoding'
